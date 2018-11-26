274669ce68b38893f7db1882ba22996b0f2ceb50
Roy Berner <r.p.berner@me.com>
2018-11-26 22:18:32 +0100

    Initial Commit

---
+++ connect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  connect
+//
+//  Created by Roy Berner on 26/11/2018.
+//  Copyright © 2018 Roy Berner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
